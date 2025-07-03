60a97ea15251450948fc81099b88022bec6cb1fd
miho411 <mihoalkurmaji@gmail.com>
2025-07-04 00:41:16 +0200

    second commit

---
+++ database/migrations/2025_07_02_152256_create_reviews_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('course_id')->constrained()->onDelete('cascade');
+            $table->tinyInteger('rating'); // من 1 إلى 5
+            $table->text('review_text')->nullable();
+            $table->unique(['user_id', 'course_id']);   //  لمنع المستخدم من انشاء اكثر من مراجعة
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
